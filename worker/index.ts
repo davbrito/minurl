eff8c0766a31f2019adb467ea0dab758746d27d5
David Brito <39559632+davbrito@users.noreply.github.com>
2025-07-23 05:43:32 +0000

    chore: refactor

--- worker/index.ts
+++ worker/index.ts
@@ -5,6 +5,7 @@ import { createContext } from "./rpc/context";
 import { appRouter } from "./rpc/router";
 import { visitUrl } from "./shortener";
 import type { ServerEnv } from "./types";
+import { apiKeyAuth } from "./middleware";
 
 const app = new Hono<ServerEnv>();
 
@@ -16,7 +17,7 @@ app.get("/x/:id", async (ctx) => {
 
   if (!id) {
     return ctx.text("Invalid request. Please provide an id in the url.", {
-      status: 400,
+      status: 400
     });
   }
 
@@ -24,26 +25,26 @@ app.get("/x/:id", async (ctx) => {
     ctx.executionCtx,
     env.MinifiedUrls,
     id,
-    req.header("referer"),
+    req.header("referer")
   );
 
   if (fullUrl === null) {
     return ctx.text("Invalid request. Please provide a valid id in the url.", {
-      status: 400,
+      status: 400
     });
   }
 
   return ctx.redirect(fullUrl, 303);
 });
 
-app.use("/rpc/*", async (c) => {
+app.use("/rpc/*", apiKeyAuth({ soft: true }), async (c) => {
   return fetchRequestHandler({
     endpoint: "/trpc",
     req: c.req.raw,
     router: appRouter,
     createContext() {
       return createContext(c);
-    },
+    }
   });
 });
 
--- worker/rpc/context.ts
+++ worker/rpc/context.ts
@@ -1,16 +1,14 @@
 import type { Context } from "hono";
-import { apiKeyAuth } from "../middleware";
 import type { ServerEnv } from "../types";
 
 export const createContext = async (
   //   ops: FetchCreateContextFnOptions,
-  hono: Context<ServerEnv>,
+  hono: Context<ServerEnv>
 ) => {
-  apiKeyAuth({ soft: true })(hono, async () => {});
   return {
     isAuthenticated: hono.get("isAuthenticated") ?? false,
     env: hono.env,
-    hono: hono,
+    hono: hono
   };
 };
 
