81709000c5d2e1e5c8374b63902deab3a67a7012
David Brito <39559632+davbrito@users.noreply.github.com>
2025-07-23 04:47:46 +0000

    chore: update copyright notice

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -18,19 +18,31 @@ function App() {
       <Route path="/_internal">
         <Inspect />
       </Route>
-      <footer className="border-t border-zinc-200/50 bg-zinc-50 inset-shadow-sm">
-        <div className="flex items-center justify-center gap-2 p-2 text-sm">
-          <span>
-            Made with {"❤️"} by{" "}
-            <a
-              href="https://github.com/davbrito"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-zinc-700 underline hover:text-zinc-900"
-            >
-              davbrito
-            </a>
-          </span>
+      <footer className="border-t border-zinc-200/50 bg-zinc-50 p-2 text-center inset-shadow-sm">
+        <div className="text-sm">
+          Made with {"❤️"} by{" "}
+          <a
+            href="https://github.com/davbrito"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-zinc-700 underline hover:text-zinc-900"
+          >
+            davbrito
+          </a>
+        </div>
+        <div className="text-xs">
+          Source code on{" "}
+          <a
+            href="https://github.com/davbrito/minurl"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-zinc-700 underline hover:text-zinc-900"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="text-xs">
+          © {new Date().getFullYear()} David Brito. All rights reserved.
         </div>
       </footer>
     </main>
