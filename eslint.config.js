d9f4dcec8622670e55288379a8dcc218a1dd7de2
David Brito <39559632+davbrito@users.noreply.github.com>
2025-07-26 03:16:08 +0000

    refactor: update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,30 @@
+// @ts-check
 import js from "@eslint/js";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default defineConfig([
+export default tseslint.config([
+  {
+    ignores: ["node_modules/", "dist/", "build/", "worker-configuration.d.ts"]
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: [js.configs.recommended]
   },
   {
     files: ["src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: { globals: { ...globals.browser } },
+    languageOptions: { globals: { ...globals.browser } }
   },
   {
     files: [
       "worker/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
       "eslint.config.js",
-      "vite.config.ts",
+      "vite.config.ts"
     ],
-    languageOptions: { globals: { ...globals.node } },
+    languageOptions: { globals: { ...globals.node } }
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
@@ -32,13 +35,13 @@ export default defineConfig([
       "@typescript-eslint/no-explicit-any": 0,
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      ]
     },
     settings: {
       react: {
-        version: "detect",
-      },
-    },
-  },
+        version: "detect"
+      }
+    }
+  }
 ]);
