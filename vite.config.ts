1aa6399e1d5e6045279d852128845a6fbd97fad0
David Brito <39559632+davbrito@users.noreply.github.com>
2026-06-13 14:29:09 +0000

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,13 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import devtoolsJson from "vite-plugin-devtools-json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: { tsconfigPaths: true },
   plugins: [
-    devtoolsJson({ uuid: "38d6a7ee-2322-4d51-97ec-a0e65857b315" }),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     reactRouter(),
-    tailwindcss(),
-    tsconfigPaths()
-  ],
-  build: { minify: false },
-  environments: {
-    ssr: {
-      build: { minify: false }
-    }
-  }
+    tailwindcss()
+  ]
 });
